3ecee1ca35a59a7ce3959b22126934f3ef2b1ae5
Owen Utley <90651143+owenutley@users.noreply.github.com>
2026-04-19 11:50:40 -0600

    Add birthday surprise button functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+// Grab the button element from your HTML
+const surpriseBtn = document.getElementById('surpriseBtn');
+
+// Add an action for when the button is clicked
+surpriseBtn.addEventListener('click', function() {
+    
+    // You can replace this simple alert with something more complex later
+    // like a confetti animation or revealing a hidden video.
+    alert('Happy Birthday, Mom! I love you!');
+    
+});
